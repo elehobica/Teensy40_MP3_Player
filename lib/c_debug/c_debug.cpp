3e6ca882b8f69b8129ae3fbf090eddc3d0353bd5
elehobica <elehobica@gmail.com>
2020-07-01 23:38:46 +0900

    fix: Ignore ERR_MP3_MAINDATA_UNDERFLOW to rescue edited MP3

---
+++ lib/c_debug/c_debug.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+// For Debug from C
+void print(const char *str)
+{
+    Serial.print(str);
+}
+
+void println(const char *str)
+{
+    Serial.println(str);
+}
---
+++ lib/c_debug/c_debug.h
@@ -0,0 +1,15 @@
+#ifndef __C_DEBUG_H_INCLUDED__
+#define __C_DEBUG_H_INCLUDED__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void print(const char *str);
+void println(const char *str);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
